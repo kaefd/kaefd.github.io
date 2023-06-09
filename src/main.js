6aa5b796aaf5731e821d696bab6f93933ffaf6cd
kaefd <ikfdla13@gmail.com>
2023-06-09 16:36:36 +0700

    prod

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
@@ -13,7 +12,6 @@ loadFonts();
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 app.use(vuetify)
 app.use(VueSweetalert2)
