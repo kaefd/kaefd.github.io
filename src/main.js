b60744ec1db4c001f5a07c4abe7b1cebbc026150
kaefd <ikfdla13@gmail.com>
2023-06-09 16:48:44 +0700

    remv

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
-import VueSweetalert2 from 'vue-sweetalert2';
 
 loadFonts();
 
@@ -12,5 +11,4 @@ const app = createApp(App)
 
 app.use(router)
 app.use(vuetify)
-app.use(VueSweetalert2)
 app.mount('#app')
