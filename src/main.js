7e69991a7c5ee41458aff73293a01d91a2006e18
kaefd <ikfdla13@gmail.com>
2023-06-09 16:47:00 +0700

    rmv swt

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import router from './router'
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
 
 loadFonts();
 
