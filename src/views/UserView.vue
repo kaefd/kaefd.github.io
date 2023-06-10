8fda6fee22e45e85e49b1271b6332b2f0ff7e3bd
kaefd <ikfdla13@gmail.com>
2023-06-10 15:55:33 +0700

    rebuild

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { defineComponent } from 'vue';
-// import api from '../api';
+import api from '../api';
 import AppBar from '../components/AppBar.vue';
 
 </script>
@@ -15,37 +15,77 @@ import AppBar from '../components/AppBar.vue';
       return {
         drawer: false,
         pageTitle: 'DATA USER',
+        tab: null,
         username: '',
         password: '',
+        otority: '',
+        dialogotoritas: [],
+        userselect: 'admin',
         open: ['barang', 'Laporan'],
         scrollInvoked: 0,
         items: '',
         jns_otoritas: ['Data Barang', 'Data Pelanggan', 'Data Supplier', 'Data User', 'Pembelian', 'Produksi', 'Penjualan', 'Pengiriman', 'Laporan' ],
         user_otoritas: '',
+        headers: [
+          {title: "Username", key: "username"},
+        ],
+        j_otoritas: [
+          {title: 'Data Barang', value: ['Tambah Barang', 'Ubah Barang', 'Hapus Barang']},
+          {title: 'Pelanggan', value: ['Tambah Pelanggan Baru', 'Ubah Pelanggan', 'Hapus Pelanggan']},
+          {title: 'Supplier', value: []},
+          {title: 'User', value: []},
+          {title: 'Pembelian', value: []},
+          {title: 'Produksi', value: []},
+          {title: 'Penjualan', value: []},
+          {title: 'Pengiriman', value: []},
+          {title: 'Laporan', value: ['Laporan Stok Barang', 'Laporan Pemasukan', 'Laporan Produksi', 'Laporan Pengeluaran', 'Laporan Pengiriman', 'Laporan Log User']},
+        ]
         
       }
     },
     created() {
-        // api.getData('/user?status=true')
-        // .then(response => {
-        //   this.items = response.data
-        // })
-        // .catch(() => {
-        //   window.location.href = '/login'
-        // })
-        // api.getData('/user_otoritas?username=admin')
-        // .then(response => {
-        //   this.user_otoritas = response.data
-        // })
-        // .catch(() => {
-        //   window.location.href = '/login'
-        // })
+        
     },
     computed: {
       // JENIS OTORITAS
-      otoritas(){
-        return  {
-          
+
+        
+    },
+      methods: {
+        getUser() {
+          api.getData('/user?status=true')
+          .then(response => {
+            this.items = response.data
+          })
+          .catch(() => {
+            window.location.href = '/login'
+          })
+        },
+        getOtoritas(){
+          const apiUrl = '/user_otoritas?'
+          const params = {
+            username: this.userselect,
+          }
+          api.getData(apiUrl, { params })
+          .then(response => {
+            this.user_otoritas = response.data
+          })
+          .catch(() => {
+            window.location.href = '/login'
+          })
+        },
+        page(){
+          return this.$emit('page', this.pageTitle)
+        },
+        dataUser(u, p) {
+          this.username = u
+          this.password = p
+        },
+        onScroll () {
+          this.scrollInvoked++
+        },
+        otoritas(){
+        return {
           'Data Barang' :
           [
             { jenisotoritas: this.user_otoritas[4].jenis_otoritas, status: this.user_otoritas[4].status },
@@ -104,73 +144,131 @@ import AppBar from '../components/AppBar.vue';
           ],
   
         }
-
-      }
-        
-    },
-      methods: {
-        page(){
-          return this.$emit('page', this.pageTitle)
         },
-        dataUser(u, p) {
-          this.username = u
-          this.password = p
+        userotoritas(user, jns) {
+          for (let i = 0; i < this.user_otoritas.length; i++) {
+            for (let j = 0; j < this.j_otoritas.length; j++) {
+              for (let k = 0; k < this.jns_otoritas.length; k++) {
+                if(user == this.user_otoritas[i].username && jns == this.jns_otoritas[k])
+                {
+                  if(this.j_otoritas[j].title == jns) {
+                    return this.j_otoritas[j].value
+                  }
+                }
+              }
+          }
+         }
         },
-        onScroll () {
-          this.scrollInvoked++
-        },
+        updt() {
+          this.getOtoritas()
+          this.getUser()
+        }
     },
     mounted() {
       this.page()
+      this.getOtoritas()
+      this.getUser()
     }
   })
 </script>
 <template>
 <v-container>
   <AppBar v-if="pageTitle != null" :pageTitle="pageTitle"/>
-  <v-row no-gutters class="rounded-t-xl align-center mt-n4 mb-2">
-    <v-responsive class="d-flex align-center mb-sm-0 mb-1 me-sm-2 me-0" width="200" max-width="400">
+  <v-row class="d-flex w-100">
+  <v-col>
+    <v-card class="elevation-0 pa-3 vh-75 rounded-lg bg-transparent" min-width="250">
       <v-btn class="rounded-xl bg-blue-custom text-white elevation-0 text-caption" height="42" width="150">Tambah Baru</v-btn>
-    </v-responsive>
-  </v-row>
-  <v-row class="d-flex">
-    <v-responsive class="me-lg-0 me-2 mb-md-0 mb-2" max-width="400">
-      <v-container class="w-100 border rounded-xl vh-75">
-        <v-span class="text-caption font-weight-bold pa-3">KODE USER</v-span>
-        <v-list class="bg-transparent">
-          <v-list-item class="text-caption" density="compact">admin</v-list-item>
-          <v-list-item class="text-caption" density="compact">beacukai</v-list-item>
-        </v-list>
-      </v-container>
-    </v-responsive>
-    <v-responsive class="me-lg-0 me-2 mb-md-0 mb-2" max-width="400">
-      <v-container class="w-100 border rounded-xl vh-75">
-        <v-span class="text-caption font-weight-bold">DETAIL USER</v-span>
-        <v-div class="d-flex">
-          <v-container class="border rounded-xl mt-5 me-2">
-            <v-div class="d-flex flex-column justify-center align-center mt-5">
-              <v-avatar size="75">
-                <v-icon color="grey-darken-1" size="75">mdi-account-circle</v-icon>
-              </v-avatar>
-            </v-div>
-            <v-div class="d-flex mt-2">
-                <v-list class="bg-transparent" density="compact">
-                <v-list-item class="text-caption font-weight-bold">Username</v-list-item>
-                <v-list-item class="text-caption font-weight-bold">Pasword</v-list-item>
-              </v-list>
-              <v-list class="bg-transparent" density="compact">
-                <v-list-item class="text-caption">Admin</v-list-item>
-                <v-list-item class="text-caption"></v-list-item>
-              </v-list>
-            </v-div>
-          </v-container>
-          <v-container class="border rounded-xl mt-5">
-            <v-span class="text-caption font-weight-bold">OTOTRITAS</v-span>
-          </v-container>
-        </v-div>
-      </v-container>
-    </v-responsive>
-  </v-row>
+      <v-list class="bg-transparent">
+        <v-list-item
+          class="text-caption font-weight-bold"
+          >
+          KODE USER
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item
+        v-for="item, i in items" :key="i"
+        class="text-caption" density="compact"
+        @click="userselect = item.username"
+        >
+        {{ item.username }}
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-col>
+  <v-col cols="4">
+    <v-card class="elevation-0 pt-5 h-100 rounded-lg bg-grey-lighten-4" min-width="320" max-width="300">
+      <p class="h5 text-center text-blue-darken-4 mb-5">DETAILS USER</p>
+      <v-text-field
+        label="Username"
+        v-model="userselect"
+        density="compact"
+        variant="outlined"
+        class="w-75 mx-auto"
+        readonly
+        />
+      <v-text-field
+        label="Password"
+        v-model="password"
+        type="password"
+        density="compact"
+        variant="outlined"
+        class="w-75 mx-auto"
+        readonly
+        />
+      <p class="w-100 bg-blue-custom pa-1 text-center text-button text-white">JENIS OTORITAS</p>
+      <v-list
+      v-scroll.self="onScroll"
+      class="overflow-y-auto bg-transparent"
+      max-height="300">
+        
+        <!-- <v-for v-for="o, i in jns_otoritas" :key="i">
+          <v-list-group :value="o">
+            <template v-slot:activator="{ props }">
+              <v-list-item
+                  v-bind="props"
+                  class="me-0 ms-auto"
+                  height="5"
+                ><v-checkbox color="blue" :model-value="true" class="mt-5" :label="o.title"></v-checkbox></v-list-item>
+              </template>
+    
+              <v-if v-if=" otoritas[i].action != '' ">
+                <v-checkbox
+                  v-for="act, a in otoritas[i].action" :key="a"
+                  color="blue"
+                  :model-value="act.check"
+                  :label="act.title"
+                  class="ms-6 mb-n6"
+                >
+                </v-checkbox>
+              </v-if>
+          </v-list-group>
+        </v-for> -->
+        <v-list-item
+          v-for="jns, i in jns_otoritas" :key="i"
+          class="text-caption"
+          density="compact"
+          @click="dialogotoritas[i] = true"
+        >
+        <v-checkbox density="compact" class="mb-n7" :label="jns" :model-value="true" readonly></v-checkbox>
+        <v-dialog v-model="dialogotoritas[i]" @update="dialogotoritas" max-width="400">
+          <v-card class="pa-5">
+            <v-span class="text-caption font-weight-bold">Otoritas</v-span>
+            <v-divider></v-divider>
+              <v-checkbox
+                class="text-caption mb-n7"
+                density="compact"
+                v-for="oto, j in userotoritas('admin', jns)" :key="j"
+                :label="oto"
+                :model-value="true"
+                readonly
+              ></v-checkbox>
+          </v-card>
+        </v-dialog>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-col>
+</v-row>
 
 
 </v-container>
