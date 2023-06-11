e4800653f888d6ea664e19b1bb9eef9c78cc70e6
kaefd <ikfdla13@gmail.com>
2023-06-12 06:15:59 +0700

    a

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ import OutReport from '../views/OutReport.vue'
 import LogView from '../views/LogView.vue'
 
 const router = createRouter({
-  base: '/',
+  base: 'https://kaefd.github.io',
   mode : 'hash',
   history: createWebHistory(),
   routes: [
