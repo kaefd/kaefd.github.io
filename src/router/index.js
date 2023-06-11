64a390c299238c718aaf911394d2a4e032cc5f0b
kaefd <ikfdla13@gmail.com>
2023-06-12 05:02:26 +0700

    ds

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ import OutReport from '../views/OutReport.vue'
 import LogView from '../views/LogView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
