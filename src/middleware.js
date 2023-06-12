b370b8e1356df77b0a805365899d86d3e7b2a2ef
kaefd <ikfdla13@gmail.com>
2023-06-12 10:20:58 +0700

    q

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 
 export default function ({ next, router }) {
   if (localStorage.getItem('token') == false) {
-    return router.push('/login');
+    return router.push('/#/login');
   }
 
   return next();
