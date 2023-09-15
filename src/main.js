4ac55cbf6d0044898a50f2d41f188bbdeae9102c
kaefd <ikfdla13@gmail.com>
2023-09-15 10:52:11 +0700

    upadate

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import router from './router'
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import "./assets/main.css"
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
 loadFonts();
 
 const app = createApp(App)
+app.component('VueDatePicker', VueDatePicker);
 app.use(router)
 app.use(vuetify)
 app.mount('#app')
