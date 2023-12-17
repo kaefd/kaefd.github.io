d240d9979c9fffe5411928ee1ac15ad676da0ac2
kaefd <ikfdla13@gmail.com>
2023-12-18 00:17:37 +0700

    base

--- src/router/index.ts
+++ src/router/index.ts
@@ -185,8 +185,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  // history: createWebHashHistory(),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
